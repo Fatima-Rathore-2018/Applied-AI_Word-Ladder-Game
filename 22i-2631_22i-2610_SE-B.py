9ee8ac191d032b06d37a536a4f569e49991ebf97
Fatima-Rathore-2018 <rathore.fatima.27@gmail.com>
2025-02-15 12:07:56 +0500

    Added Main Function

--- 22i-2631_22i-2610_SE-B.py
+++ 22i-2631_22i-2610_SE-B.py
@@ -1,17 +1,5 @@
 import json
 
-#Reading words from words_dictionary.json
-with open("words_dictionary.json", "r") as words_dictonary:
-    wordsData = json.load(words_dictonary)
-
-#Creating a dictionary for the game.
-WordLadderDictionary = {}
-
-#Filtering out words with length greater than equal to 3 and less than equal to 6.
-for word in wordsData: 
-    if 3 <= len(word) <= 6:
-        WordLadderDictionary[word] = wordsData[word]
-
 #Creation of Class Node
 class WordNode:
     def __init__(self, state, parent, actions, pathCost):
@@ -22,3 +10,20 @@ class WordNode:
 
 #Creation of Graph
 WordLadderGraph = {}
+
+# Main Function.
+def main():
+    #Reading words from words_dictionary.json
+    with open("words_dictionary.json", "r") as words_dictonary:
+        wordsData = json.load(words_dictonary)
+
+    #Creating a dictionary for the game.
+    WordLadderDictionary = {}
+
+    #Filtering out words with length greater than equal to 3 and less than equal to 6.
+    for word in wordsData: 
+        if 3 <= len(word) <= 6:
+            WordLadderDictionary[word] = wordsData[word]
+
+if __name__ == "__main__":
+    main()
